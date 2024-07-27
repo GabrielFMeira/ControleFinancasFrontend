0fc2bd3551fde25aa521cac163442430e179e54f
Gabriel da Fonseca Meira <gabriel.gfm@outlook.com>
2024-07-27 20:42:20 -0300

    primeiro commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="navbar-container">
+        <div id="btn-container">
+            <router-link to="/">Home</router-link>
+            <router-link to="/contas">Contas</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+
+    #navbar-container {
+        height: 50px;
+        background-color: aqua;
+        display: flex;
+        padding: 15px;
+        text-decoration: none;
+    }
+
+</style>
